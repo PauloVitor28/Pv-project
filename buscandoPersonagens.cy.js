1087a92c819078af600e92a7ca5e38d3cd7617ae
PauloVitor28 <79941277+PauloVitor28@users.noreply.github.com>
2023-02-02 10:01:44 -0300

    Add files via upload

---
+++ buscandoPersonagens.cy.js
@@ -0,0 +1,33 @@
+
+describe('Pessonagens ', () => {
+
+  const baseUrl = "https://rickandmortyapi.com/api"
+  it('listar o nome e especie todos os perssonagens da familia - Sucesso', () => {
+    cy.request({
+      method: 'GET',
+      url: baseUrl + '/character',
+      headers: { 'Accept-Language': 'pt-br', },
+    }).then((response)=>{
+    
+      expect(response.status).to .eq(200)
+      expect(response.body).is.not.null 
+      expect(response.body.results[0].name).to .eq("Rick Sanchez")
+      expect(response.body.results[0].species).to .eq("Human")
+      expect(response.body.results[1].name).to .eq("Morty Smith")
+      expect(response.body.results[1].species).to .eq("Human")
+      expect(response.body.results[2].name).to .eq("Summer Smith")
+      expect(response.body.results[2].species).to .eq("Human")
+      expect(response.body.results[3].name).to .eq("Beth Smith")
+      expect(response.body.results[3].species).to .eq("Human")
+      expect(response.body.results[4].name).to .eq("Jerry Smith")
+      expect(response.body.results[4].species).to .eq("Human")
+    });
+      
+
+
+      })
+
+
+        
+     
+    })
